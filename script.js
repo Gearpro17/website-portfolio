6b807a1580c84286abf4a07c4dddb761d7241f87
Gearpro17 <joelecossu117@gmail.com>
2026-09-05 17:27:18 +0200

    - Added new project zip file - Added scroll back to top arrow

--- script.js
+++ script.js
@@ -148,4 +148,21 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
+  const backToTop = document.getElementById("backToTop");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 500) {
+        backToTop.classList.add("visible");
+    } else {
+        backToTop.classList.remove("visible");
+    }
+  });
+
+  backToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+  });
+
 });
